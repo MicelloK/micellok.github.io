26bbbf72124b07f69942ad58b4e9d29ea673a0c5
Michał Kobiera <kobiera@zoho.com>
2023-11-23 17:13:25 +0100

    Add page, not responsive yet

---
+++ app.js
@@ -0,0 +1,20 @@
+// contact form
+
+const inputs = document.querySelectorAll(".input");
+
+function focusFunc() {
+    const parent = this.parentNode;
+    parent.classList.add("focus");
+}
+
+function blurFunc() {
+    const parent = this.parentNode;
+    if(this.value == "") {
+        parent.classList.remove("focus");
+    }
+}
+
+inputs.forEach((input) => {
+    input.addEventListener("focus", focusFunc);
+    input.addEventListener("blur", blurFunc);
+});
