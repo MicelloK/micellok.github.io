0308e01e8fab17080d8a765cf134c1e556cd5d64
Michał Kobiera <kobiera@zoho.com>
2023-11-25 17:17:03 +0100

    Add lang btns logic

--- app.js
+++ app.js
@@ -19,7 +19,53 @@ inputs.forEach((input) => {
     input.addEventListener("blur", blurFunc);
 });
 
+//dark theme button
+
 const toggleThemeBtn = document.getElementById("theme");
 toggleThemeBtn.addEventListener("click", () => {
     document.body.classList.toggle("dark");
+})
+
+// lang buttons 
+
+const resultContainer = document.querySelector("main");
+
+const plBtn = document.getElementById("pl");
+plBtn.addEventListener("click", async function() {
+    try {
+        let response = await fetch('index_pl.html');
+        let data = await response.text();
+
+        let tempDiv = document.createElement('div');
+        tempDiv.innerHTML = data;
+        let mainElement = tempDiv.querySelector("main");
+
+        if (mainElement) {
+            resultContainer.innerHTML = mainElement.innerHTML;
+        } else {
+            console.error('Main section error');
+        }
+    } catch (error) {
+        console.error('Render page error:', error);
+    }
+})
+
+const enBtn = document.getElementById("en");
+enBtn.addEventListener("click", async function() {
+    try {
+        let response = await fetch('index.html');
+        let data = await response.text();
+
+        let tempDiv = document.createElement('div');
+        tempDiv.innerHTML = data;
+        let mainElement = tempDiv.querySelector("main");
+
+        if (mainElement) {
+            resultContainer.innerHTML = mainElement.innerHTML;
+        } else {
+            console.error('Main section error');
+        }
+    } catch (error) {
+        console.error('Render page error:', error);
+    }
 })
